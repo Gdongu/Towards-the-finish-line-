fe70d5cfa6955db4a61a038a3826e9cd2687320a
Gdongu <70313873+Gdongu@users.noreply.github.com>
2025-10-23 20:49:54 +0900

    Update PlaytimeWidget.h

--- PlaytimeWidget.h
+++ PlaytimeWidget.h
@@ -12,12 +12,11 @@ class UPlaytimeWidget : public UUserWidget
     GENERATED_BODY()
 
 public:
-    /** 초 단위를 mm:ss 또는 hh:mm:ss 문자열로 바꿔 TextBlock에 표시 */
     UFUNCTION(BlueprintCallable)
     void SetTimeSeconds(float Seconds);
 
 protected:
-    /** 디자이너에서 만든 TextBlock에 바인딩 (이름: TimeText) */
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<UTextBlock> TimeText;
+
 };
